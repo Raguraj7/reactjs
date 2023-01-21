074c81a5ebc88fd72a09645085d7ff9c62ce1474
unknown <starksplick@gmail.com>
2023-01-21 23:42:32 +0530

    flex box

--- src/components/Flexbox.js
+++ src/components/Flexbox.js
@@ -1,11 +1,16 @@
 const Flexbox = () => {
-    const arr=['test','test2','test3', 'test4', 'test5','test6','test','test2','test3','test','test2','test3','test','test2','test3','test','test2','test3','test','test2','test3','test','test2','test3',]
+  const arrobj=[{name:'test',color:'amber'},{name:'test1',color:'gray'},{name:'test',color:'slate'},{name:'test',color:'blue'},{name:'test',color:'yellow'}]  
+  const arr=['test','test2','test3', 'test4', 'test5','test6','test','test2','test3','test','test2','test3','test','test2','test3','test','test2','test3','test','test2','test3','test','test2','test3',]
     return (
         <>
         <div className="flex flex-wrap gap-6 justify-center items-center m-3">
-          {arr.map((d,i)=>(
+          {/* {arr.map((d,i)=>(
             <div key={i} className='bg-indigo-500 text-white px-6 py-1 rounded-full'> {d}</div>
-          ))}  
+          ))}  */}
+
+           {arrobj.map((obj,i)=>(
+            <div key={i} className={`bg-indigo-500 ${obj.color==='amber'?'text-amber-500':obj.color==='slate'?'text-slate-500':obj.color==='blue'?'text-blue-500':'text-white'} px-6 py-1 rounded-full `}> {obj.name}</div>
+          ))} 
         </div>
         </>
     );
