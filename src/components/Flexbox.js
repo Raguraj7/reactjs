1fdc17ea1085252a7bf1f785b33c087f53261625
unknown <starksplick@gmail.com>
2023-02-05 13:22:17 +0530

    ref component

--- src/components/Flexbox.js
+++ src/components/Flexbox.js
@@ -1,19 +1,19 @@
-const Flexbox = ({arrobj}) => {
-  
-  const arr=['test','test2','test3', 'test4', 'test5','test6','test','test2','test3','test','test2','test3','test','test2','test3','test','test2','test3','test','test2','test3','test','test2','test3',]
-    return (
-        <>
-        <div className="flex flex-wrap gap-6 justify-center items-center m-3">
-          {arr.map((d,i)=>(
-            <div key={i} className='bg-indigo-500 text-white px-6 py-1 rounded-full'> {d}</div>
-          ))} 
+// const Flexbox = ({arrobj}) => {
 
-           {arrobj.map((obj,i)=>(
-            <div key={i} className={`bg-indigo-500 ${obj.color==='amber'?'text-amber-500':obj.color==='slate'?'text-slate-500':obj.color==='blue'?'text-blue-500':'text-white'} px-6 py-1 rounded-full `}> {obj.name}</div>
-          ))} 
-        </div>
-        </>
-    );
-}
+//   const arr=['test','test2','test3', 'test4', 'test5','test6','test','test2','test3','test','test2','test3','test','test2','test3','test','test2','test3','test','test2','test3','test','test2','test3',]
+//     return (
+//         <>
+//         <div className="flex flex-wrap gap-6 justify-center items-center m-3">
+//           {arr.map((d,i)=>(
+//             <div key={i} className='bg-indigo-500 text-white px-6 py-1 rounded-full'> {d}</div>
+//           ))}
 
-export default Flexbox;
+//            {arrobj.map((obj,i)=>(
+//             <div key={i} className={`bg-indigo-500 ${obj.color==='amber'?'text-amber-500':obj.color==='slate'?'text-slate-500':obj.color==='blue'?'text-blue-500':'text-white'} px-6 py-1 rounded-full `}> {obj.name}</div>
+//           ))}
+//         </div>
+//         </>
+//     );
+// }
+
+// export default Flexbox;
--- src/components/Ref.js
+++ src/components/Ref.js
@@ -1,18 +1,18 @@
-const Ref = ({title,setTitle}) => {
-    console.log(title);
-    // const changeName=()=>{
-    //    setTitle([{
-    //     ...title,Username:{
-    //         firstName:'testing',
-    //         ...title.lastname
-    //     }
-    //    }])
+const Ref = ({ title, setTitle }) => {
+  console.log(title);
+  // const changeName=()=>{
+  //    setTitle([{
+  //     ...title,Username:{
+  //         firstName:'testing',
+  //         ...title.lastname
+  //     }
+  //    }])
 
-    // }
-    
-    return (
-        <>
-        {/* <button className="m-2 p-3 bg-indigo-500 text-center" onClick={()=>setTitle([{
+  // }
+
+  return (
+    <>
+      {/* <button className="m-2 p-3 bg-indigo-500 text-center" onClick={()=>setTitle([{
         ...title,Username:{...title.Username,
             firstName:'testing',
             lastname:'idfdklhfgkdf'
@@ -20,20 +20,24 @@ const Ref = ({title,setTitle}) => {
        }])}>update</button>
        onClick={()=>setTitle(...title[{Username:{firstName:'testing',}}])}
        */}
-       {/* <button className="m-2 p-3 bg-indigo-500 text-center" onClick={()=>setTitle([title[0].={userfirstName:'testing',lastName:'testing2'}])}>update</button> */}
-        {title.map((user,i)=>(
-            <div key={i}>
-                <p> {user.Username.firstName}</p>
-                <button className="bg-zinc-500 p-1 m-3" onClick={()=>setTitle([...title],title[i].Username.firstName='trichyyyyyy')}>Update</button>
-                
-            </div>
-            
-        ))}
-        {JSON.stringify(title)}
-        {console.log('************************',title)}
-        
-        </>
-    );
-}
+      {/* <button className="m-2 p-3 bg-indigo-500 text-center" onClick={()=>setTitle([title[0].={userfirstName:'testing',lastName:'testing2'}])}>update</button> */}
+      {title.map((user, i) => (
+        <div key={i}>
+          <p> {user.Username.firstName}</p>
+          <button
+            className="bg-zinc-500 p-1 m-3"
+            onClick={() =>
+              setTitle(((title[i].Username.firstName = "jgghghfg"), [...title]))
+            }
+          >
+            Update
+          </button>
+        </div>
+      ))}
+      {JSON.stringify(title)}
+      {console.log("************************", title)}
+    </>
+  );
+};
 
 export default Ref;
